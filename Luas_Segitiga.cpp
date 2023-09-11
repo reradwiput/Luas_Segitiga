ac84ec7289886e891e6d3aab7323b787b096d7e5
reradwiput <144641062+reradwiput@users.noreply.github.com>
2023-09-11 09:33:02 +0700

    Create Luas_Segitiga.cpp

---
+++ Luas_Segitiga.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main( ) {
+int a,t,l;
+cout<<"MENGHITUNG LUAS SEGITIGA \n \n";
+cout<<"masukan alas: ";
+cin>>a;
+cout<<"masukan tinggi: ";
+cin>>t;
+l=0.5*a*t;
+cout<<"hasilnya adalah: "<<l<< endl;
+return 0;
+}
